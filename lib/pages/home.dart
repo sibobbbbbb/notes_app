c15a08e22ae18e1e79c8fb6b8b78e127315085da
sibobbbbbb <114354358+sibobbbbbb@users.noreply.github.com>
2024-09-10 23:37:16 +0700

    feat : save after editing note

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,24 +1,135 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:notes_app/components/home/body_home.dart';
+import 'package:intl/intl.dart';
 import '../components/home/title_appbar_home.dart';
 import '../components/home/actions_appbar_home.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:hive/hive.dart';
+import '../../pages/note.dart';
+import '../database/note_object.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  late Box noteBox;
+  List<dynamic> notes = [];
+
+  @override
+  void initState() {
+    super.initState();
+    noteBox = Hive.box('notes');
+    notes = noteBox.values.toList();
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    setState(() {
+      notes = noteBox.values.toList();
+    });
+  }
+
+  String getLastUpdate(Note note) {
+    DateTime inputDate = DateFormat('yyyy-MM-dd').parse(note.lastUpdate);
+    DateTime yesterday = DateTime.now().subtract(const Duration(days: 1));
+    DateTime today = DateTime.now();
+    if (inputDate.day == today.day &&
+        inputDate.month == today.month &&
+        inputDate.year == today.year) {
+      return 'Today';
+    } else if (inputDate.day == yesterday.day &&
+        inputDate.month == yesterday.month &&
+        inputDate.year == yesterday.year) {
+      return 'Yesterday';
+    } else {
+      return note.lastUpdate;
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        elevation: 0,
         backgroundColor: Colors.white,
-        title: titleAppbarHome(),
-        actions: actionsAppbarHome(),
-        systemOverlayStyle: SystemUiOverlayStyle.light,
-      ),
-      body: bodyHome(),
-    );
+        appBar: AppBar(
+          elevation: 0,
+          backgroundColor: Colors.white,
+          title: titleAppbarHome(),
+          actions: actionsAppbarHome(),
+          systemOverlayStyle: SystemUiOverlayStyle.light,
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: MasonryGridView.count(
+            crossAxisCount: 2,
+            mainAxisSpacing: 4,
+            crossAxisSpacing: 4,
+            itemCount: notes.length,
+            itemBuilder: (context, index) {
+              return Card(
+                color: Colors.grey[200],
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                child: InkWell(
+                  onTap: () async {
+                    await Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) =>
+                                NotePage(note: notes[index])));
+
+                    setState(() {
+                      notes = Hive.box('notes').values.toList();
+                    });
+                  },
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        RichText(
+                          text: TextSpan(
+                            children: [
+                              TextSpan(
+                                text: getLastUpdate(notes[index]),
+                                style: const TextStyle(
+                                  fontSize: 12,
+                                  color: Colors.black54,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              const TextSpan(
+                                text: ' - Last Update',
+                                style: TextStyle(
+                                  fontSize: 11,
+                                  color: Colors.grey,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        const SizedBox(height: 3),
+                        Text(
+                          notes[index].title!,
+                          style: const TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16,
+                          ),
+                        ),
+                        const SizedBox(height: 8),
+                        Text(notes[index].content!),
+                      ],
+                    ),
+                  ),
+                ),
+              );
+            },
+          ),
+        ));
   }
 }
--- lib/pages/note.dart
+++ lib/pages/note.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import '../database/note_object.dart';
 
 class NotePage extends StatefulWidget {
@@ -22,102 +23,113 @@ class _NotePage extends State<NotePage> {
     _controller1.text = widget.note.title;
     _controller2.text = widget.note.content;
   }
+
+  Future<bool> _onWillPop() async {
+    if (_controller1.text != widget.note.title || _controller2.text != widget.note.content) {
+      widget.note.title = _controller1.text;
+      widget.note.content = _controller2.text;
+      widget.note.lastUpdate = DateFormat('yyyy-MM-dd').format(DateTime.now());
+
+      await widget.note.save();
+    }
+
+    return true;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Create Note"),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(12.0),
-        child: Column(
-          children: [
-            // Toolbar for formatting text
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                IconButton(
-                  icon: const Icon(Icons.format_bold),
-                  onPressed: () {
-                    setState(() {
-                      _isBold = !_isBold;
-                    });
-                  },
-                  color: _isBold ? Colors.blue : Colors.black,
-                ),
-                IconButton(
-                  icon: const Icon(Icons.format_italic),
-                  onPressed: () {
-                    setState(() {
-                      _isItalic = !_isItalic;
-                    });
-                  },
-                  color: _isItalic ? Colors.blue : Colors.black,
-                ),
-                DropdownButton<double>(
-                  value: _fontSize,
-                  items: [16, 18, 20, 24].map((size) {
-                    return DropdownMenuItem<double>(
-                      value: size.toDouble(),
-                      child: Text(size.toString()),
-                    );
-                  }).toList(),
-                  onChanged: (double? newSize) {
-                    setState(() {
-                      _fontSize = newSize!;
-                    });
-                  },
-                ),
-              ],
-            ),
-            Expanded(
-              child: SingleChildScrollView(
-                child: Column(
-                  children: [
-                    // Title and description text fields
-                    TextField(
-                      controller: _controller1,
-                      maxLength: 30,
-                      buildCounter: (BuildContext context,
-                          {int? currentLength,
-                          bool? isFocused,
-                          int? maxLength}) {
-                        return null;
-                      },
-                      decoration: const InputDecoration(
-                        hintText: "Title",
-                        border: InputBorder.none,
+    return PopScope(
+      onPopInvoked: (popDisposition) async {
+        await _onWillPop();
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text("Create Note"),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  IconButton(
+                    icon: const Icon(Icons.format_bold),
+                    onPressed: () {
+                      setState(() {
+                        _isBold = !_isBold;
+                      });
+                    },
+                    color: _isBold ? Colors.blue : Colors.black,
+                  ),
+                  IconButton(
+                    icon: const Icon(Icons.format_italic),
+                    onPressed: () {
+                      setState(() {
+                        _isItalic = !_isItalic;
+                      });
+                    },
+                    color: _isItalic ? Colors.blue : Colors.black,
+                  ),
+                  DropdownButton<double>(
+                    value: _fontSize,
+                    items: [16, 18, 20, 24].map((size) {
+                      return DropdownMenuItem<double>(
+                        value: size.toDouble(),
+                        child: Text(size.toString()),
+                      );
+                    }).toList(),
+                    onChanged: (double? newSize) {
+                      setState(() {
+                        _fontSize = newSize!;
+                      });
+                    },
+                  ),
+                ],
+              ),
+              Expanded(
+                child: SingleChildScrollView(
+                  child: Column(
+                    children: [
+                      // Title and description text fields
+                      TextField(
+                        controller: _controller1,
+                        maxLength: 30,
+                        buildCounter: (BuildContext context,
+                            {int? currentLength, bool? isFocused, int? maxLength}) {
+                          return null;
+                        },
+                        decoration: const InputDecoration(
+                          hintText: "Title",
+                          border: InputBorder.none,
+                        ),
+                        style: TextStyle(
+                          fontSize: _fontSize,
+                          fontWeight: _isBold ? FontWeight.bold : FontWeight.normal,
+                          fontStyle: _isItalic ? FontStyle.italic : FontStyle.normal,
+                        ),
                       ),
-                      style: TextStyle(
-                        fontSize: _fontSize,
-                        fontWeight:
-                            _isBold ? FontWeight.bold : FontWeight.normal,
-                        fontStyle:
-                            _isItalic ? FontStyle.italic : FontStyle.normal,
+                      const Divider(),
+                      TextField(
+                        controller: _controller2,
+                        maxLines: null,
+                        style: TextStyle(
+                          fontSize: _fontSize,
+                          fontWeight: _isBold ? FontWeight.bold : FontWeight.normal,
+                          fontStyle: _isItalic ? FontStyle.italic : FontStyle.normal,
+                        ),
+                        decoration: const InputDecoration(
+                          hintText: "Start typing your note...",
+                          border: InputBorder.none,
+                        ),
                       ),
-                    ),
-                    const Divider(),
-                    TextField(
-                      controller: _controller2,
-                      maxLines: null,
-                      style: TextStyle(
-                        fontSize: _fontSize,
-                        fontWeight:
-                            _isBold ? FontWeight.bold : FontWeight.normal,
-                        fontStyle:
-                            _isItalic ? FontStyle.italic : FontStyle.normal,
-                      ),
-                      decoration: const InputDecoration(
-                        hintText: "Start typing your note...",
-                        border: InputBorder.none,
-                      ),
-                    ),
-                    const SizedBox(height: 20)
-                  ],
+                      const SizedBox(height: 20),
+                    ],
+                  ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
