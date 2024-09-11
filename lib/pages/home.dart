52ac255564266e918c42c3e38a13c877c6dedace
sibobbbbbb <114354358+sibobbbbbb@users.noreply.github.com>
2024-09-11 23:14:13 +0700

    feat : change view (list or grid)

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:intl/intl.dart';
 import '../components/home/title_appbar_home.dart';
-import '../components/home/actions_appbar_home.dart';
-import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:hive/hive.dart';
 import '../../pages/note.dart';
 import '../database/note_object.dart';
@@ -18,6 +17,145 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   late Box noteBox;
   List<dynamic> notes = [];
+  bool isGridView = true;
+
+  Widget gridView(List<dynamic> notes) {
+    return MasonryGridView.count(
+      crossAxisCount: 2,
+      mainAxisSpacing: 4,
+      crossAxisSpacing: 4,
+      itemCount: notes.length,
+      itemBuilder: (context, index) {
+        return Card(
+          color: Colors.grey[200],
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(8),
+          ),
+          child: InkWell(
+            onTap: () async {
+              await Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) =>
+                          NotePage(note: notes[index])));
+
+              setState(() {
+                notes = Hive
+                    .box('notes')
+                    .values
+                    .toList();
+              });
+            },
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  RichText(
+                    text: TextSpan(
+                      children: [
+                        TextSpan(
+                          text: getLastUpdate(notes[index]),
+                          style: const TextStyle(
+                            fontSize: 12,
+                            color: Colors.black54,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        const TextSpan(
+                          text: ' - Last Update',
+                          style: TextStyle(
+                            fontSize: 11,
+                            color: Colors.grey,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(height: 3),
+                  Text(
+                    notes[index].title!,
+                    style: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16,
+                    ),
+                  ),
+                  const SizedBox(height: 8),
+                  Text(notes[index].content!),
+                ],
+              ),
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+  Widget listView(List<dynamic> notes) {
+    return ListView.builder(
+        itemCount: notes.length,
+        itemBuilder: (BuildContext context, int index) {
+          return Card(
+            color: Colors.grey[200],
+            child: InkWell(
+                onTap: () async {
+                  await Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) =>
+                              NotePage(note: notes[index])));
+
+                  setState(() {
+                    notes = Hive
+                        .box('notes')
+                        .values
+                        .toList();
+                  });
+                },
+                child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        RichText(
+                          text: TextSpan(
+                            children: [
+                              TextSpan(
+                                text: getLastUpdate(notes[index]),
+                                style: const TextStyle(
+                                  fontSize: 12,
+                                  color: Colors.black54,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              const TextSpan(
+                                text: ' - Last Update',
+                                style: TextStyle(
+                                  fontSize: 11,
+                                  color: Colors.grey,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        const SizedBox(height: 3),
+                        Text(
+                          notes[index].title!,
+                          style: const TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16,
+                          ),
+                        ),
+                        const SizedBox(height: 8),
+                        Text(notes[index].content!),
+                      ],
+                    )
+                )
+            ),
+          );
+        }
+    );
+  }
 
   @override
   void initState() {
@@ -59,96 +197,70 @@ class _HomeState extends State<Home> {
           elevation: 0,
           backgroundColor: Colors.white,
           title: titleAppbarHome(),
-          actions: actionsAppbarHome(),
+          actions: <Widget>[
+            Container(
+              padding: const EdgeInsets.all(1),
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: Colors.grey[200],
+              ),
+              child: IconButton(
+                color: Colors.black,
+                icon: const Icon(Icons.search),
+                onPressed: () {
+                  // print('Search');
+                },
+              ),
+            ),
+            const SizedBox(width: 15),
+            Container(
+              padding: const EdgeInsets.all(1),
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: Colors.grey[200],
+              ),
+              child: IconButton(
+                color: Colors.black,
+                icon: isGridView
+                    ? const Icon(Icons.grid_view)
+                    : const Icon(Icons.list),
+                onPressed: () {
+                  setState(() {
+                    isGridView = !isGridView;
+                  });
+                },
+              ),
+            ),
+            const SizedBox(width: 10),
+          ],
           systemOverlayStyle: SystemUiOverlayStyle.light,
         ),
         floatingActionButton: FloatingActionButton(
-            onPressed: () async {
-              await Navigator.push(context, MaterialPageRoute(
-                builder: (context) => NotePage(note: Note(
-                  title: '',
-                  content: '',
-                  lastUpdate: DateFormat('yyyy-MM-dd').format(DateTime.now()),
-                ))
-              ));
-              setState(() {
-                notes = Hive.box('notes').values.toList();
-              });
-            },
-            backgroundColor: Colors.grey[200],
-            child: const Icon(Icons.add),
+          onPressed: () async {
+            await Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) =>
+                        NotePage(
+                            note: Note(
+                              title: '',
+                              content: '',
+                              lastUpdate:
+                              DateFormat('yyyy-MM-dd').format(DateTime.now()),
+                            ))));
+            setState(() {
+              notes = Hive
+                  .box('notes')
+                  .values
+                  .toList();
+            });
+          },
+          backgroundColor: Colors.grey[200],
+          child: const Icon(Icons.add),
         ),
         body: Padding(
           padding: const EdgeInsets.all(8.0),
-          child: MasonryGridView.count(
-            crossAxisCount: 2,
-            mainAxisSpacing: 4,
-            crossAxisSpacing: 4,
-            itemCount: notes.length,
-            itemBuilder: (context, index) {
-              return Card(
-                color: Colors.grey[200],
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(8),
-                ),
-                child: InkWell(
-                  onTap: () async {
-                    await Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) =>
-                                NotePage(note: notes[index])));
-
-                    setState(() {
-                      notes = Hive
-                          .box('notes')
-                          .values
-                          .toList();
-                    });
-                  },
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        RichText(
-                          text: TextSpan(
-                            children: [
-                              TextSpan(
-                                text: getLastUpdate(notes[index]),
-                                style: const TextStyle(
-                                  fontSize: 12,
-                                  color: Colors.black54,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                              const TextSpan(
-                                text: ' - Last Update',
-                                style: TextStyle(
-                                  fontSize: 11,
-                                  color: Colors.grey,
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                        const SizedBox(height: 3),
-                        Text(
-                          notes[index].title!,
-                          style: const TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 16,
-                          ),
-                        ),
-                        const SizedBox(height: 8),
-                        Text(notes[index].content!),
-                      ],
-                    ),
-                  ),
-                ),
-              );
-            },
-          ),
+          child: isGridView ? gridView(notes) : listView(notes),
         ));
   }
 }
