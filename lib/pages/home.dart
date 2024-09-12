b522daf52b64e0304b63dabb8db9242f7d3907ea
sibobbbbbb <114354358+sibobbbbbb@users.noreply.github.com>
2024-09-12 19:30:35 +0700

    feat : implement search bar

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -18,6 +18,9 @@ class _HomeState extends State<Home> {
   late Box noteBox;
   List<dynamic> notes = [];
   bool isGridView = true;
+  bool isSearching = false;
+  bool isAnimating = false;
+  final TextEditingController searchController = TextEditingController();
 
   void stateFunc() {
     setState(() {
@@ -51,61 +54,128 @@ class _HomeState extends State<Home> {
             padding: const EdgeInsets.only(top: 2),
             child: Row(
               children: [
-                Container(
+                AnimatedContainer(
                   padding: const EdgeInsets.all(3),
                   decoration: BoxDecoration(
                     color: Colors.blueAccent,
                     borderRadius: BorderRadius.circular(50),
                   ),
+                  duration: const Duration(milliseconds: 300),
+                  width: isSearching ? 52 : 213,
                   child: Row(
                     children: [
                       const CircleAvatar(
                         radius: 23,
                         backgroundImage: AssetImage('assets/images/SiBoB.png'),
                       ),
-                      const SizedBox(width: 15),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          const Text(
-                            'Farhan Raditya',
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 18,
-                              fontWeight: FontWeight.bold,
+                      if (!isSearching && !isAnimating) const SizedBox(width: 15),
+                      if (!isSearching && !isAnimating)
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            const Text(
+                              'Farhan Raditya',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold,
+                              ),
                             ),
-                          ),
-                          Text(
-                            '${notes.length} Notes',
-                            style: const TextStyle(
-                              color: Colors.white,
-                              fontSize: 12,
-                              fontWeight: FontWeight.bold,
+                            Text(
+                              '${notes.length} Notes',
+                              style: const TextStyle(
+                                color: Colors.white,
+                                fontSize: 12,
+                                fontWeight: FontWeight.bold,
+                              ),
                             ),
-                          ),
-                        ],
-                      ),
-                      const SizedBox(width: 25),
+                          ],
+                        ),
+                      if (!isSearching && !isAnimating) const SizedBox(width: 25),
                     ],
                   ),
                 ),
+                if (!isSearching) const SizedBox(width: 37) else const SizedBox(width: 13),
+                if (!isSearching)
+                  Container(
+                    padding: const EdgeInsets.all(1),
+                    decoration: const BoxDecoration(
+                        shape: BoxShape.circle, color: Color(0xFF3B3B3B)),
+                    child: IconButton(
+                      color: Colors.white,
+                      icon: const Icon(Icons.search),
+                      onPressed: () {
+                        setState(() {
+                          isSearching = !isSearching;
+                        });
+                      },
+                    ),
+                  )
+                else
+                  Expanded(
+                    child: TextField(
+                      controller: searchController,
+                      decoration: InputDecoration(
+                        filled: true,
+                        fillColor: Colors.white,
+                        hintText: "Search notes...",
+                        hintStyle: const TextStyle(color: Colors.black45),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(30),
+                          borderSide: BorderSide.none,
+                        ),
+                        contentPadding: const EdgeInsets.symmetric(
+                          horizontal: 20,
+                          vertical: 15,
+                        ),
+                      ),
+                      style: const TextStyle(color: Colors.black),
+                      onChanged: (value) {
+                        setState(() {
+                          notes = noteBox.values
+                              .where((note) =>
+                                  note.title
+                                      .toString()
+                                      .toLowerCase()
+                                      .contains(value.toLowerCase()) ||
+                                  note.content
+                                      .toString()
+                                      .toLowerCase()
+                                      .contains(value.toLowerCase()))
+                              .toList();
+                        });
+                      },
+                    ),
+                  ),
+
+                if (isSearching)
+                  IconButton(
+                    icon: const Icon(Icons.close, color: Colors.white),
+                    onPressed: () {
+                      if (isSearching) {
+                        setState(() {
+                          isSearching = false;
+                          isAnimating = true;
+                          searchController.clear();
+                          stateFunc();
+                        });
+
+                        Future.delayed(const Duration(milliseconds: 300), () {
+                          setState(() {
+                            isAnimating = false;
+                          });
+                        });
+                      } else {
+                        setState(() {
+                          isSearching = true;
+                        });
+                      }
+                    },
+                  ),
               ],
             ),
           ),
           actions: <Widget>[
-            Container(
-              padding: const EdgeInsets.all(1),
-              decoration: const BoxDecoration(
-                  shape: BoxShape.circle, color: Color(0xFF3B3B3B)),
-              child: IconButton(
-                color: Colors.white,
-                icon: const Icon(Icons.search),
-                onPressed: () {
-                  // print('Search');
-                },
-              ),
-            ),
-            const SizedBox(width: 15),
             Container(
               padding: const EdgeInsets.all(1),
               decoration: const BoxDecoration(
@@ -149,7 +219,9 @@ class _HomeState extends State<Home> {
         ),
         body: Padding(
           padding: const EdgeInsets.all(8.0),
-          child: isGridView ? gridView(notes,stateFunc) : listView(notes,stateFunc),
+          child: isGridView
+              ? gridView(notes, stateFunc)
+              : listView(notes, stateFunc),
         ));
   }
 }
