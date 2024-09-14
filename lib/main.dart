c4556a81ed8d24f9f04a33817098153630aa0afc
sibobbbbbb <114354358+sibobbbbbb@users.noreply.github.com>
2024-09-14 19:04:27 +0700

    feat : implement login using google sign in

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,21 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/adapters.dart';
 import 'package:hive_flutter/hive_flutter.dart';
-import 'pages/home.dart';
+import 'package:notes_app/pages/login.dart';
+import 'package:notes_app/utils/firebase_options.dart';
 import 'database/note_object.dart';
 
 Future<void> main() async {
+  // Initialize Hive
   await Hive.initFlutter();
   Hive.registerAdapter(NoteAdapter());
   await Hive.openBox("Notes");
+
+  // Initialize Firebase
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
   runApp(const MyApp());
 }
 
@@ -18,7 +26,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Home(),
+      home: Login(),
     );
   }
 }
---
+++ lib/pages/login.dart
@@ -0,0 +1,103 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:sign_in_button/sign_in_button.dart';
+
+class Login extends StatefulWidget {
+  const Login({super.key});
+
+  @override
+  State<Login> createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  User? _user;
+
+  @override
+  void initState() {
+    super.initState();
+    _auth.authStateChanges().listen((User? user) {
+      setState(() {
+        _user = user;
+      });
+    });
+  }
+
+  Widget _signInButton() {
+    return SizedBox(
+        height: 50,
+        child: SignInButton(
+          shape: const RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(Radius.circular(10))),
+          Buttons.google,
+          onPressed: _handleGoogleSignIn,
+        ));
+  }
+
+  void _handleGoogleSignIn() {
+    try {
+      GoogleAuthProvider googleAuthProvider = GoogleAuthProvider();
+      _auth.signInWithProvider(googleAuthProvider);
+    } catch (e) {
+      if (kDebugMode) {
+        print(e);
+      }
+    }
+  }
+
+  Widget displayUser() {
+    return SizedBox(
+      width: MediaQuery.of(context).size.width,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisSize: MainAxisSize.max,
+        children: [
+          Container(
+            height: 100,
+            width: 100,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: NetworkImage(_user!.photoURL!),
+              ),
+            ),
+          ),
+          Text(_user!.email!),
+          Text(_user!.displayName ?? ""),
+          MaterialButton(
+            color: Colors.red,
+            onPressed: _auth.signOut,
+            child: const Text("Sign Out"),
+          )
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    double halfHeightPhone = MediaQuery.of(context).size.height / 2;
+    return Scaffold(
+      backgroundColor: const Color(0xFF252525),
+      body: Padding(
+        padding: EdgeInsets.only(top: halfHeightPhone - 100),
+        child: Center(
+          child: Column(
+            children: [
+              const Text(
+                'Login',
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 40,
+                    fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 20),
+              if (_user != null) displayUser() else _signInButton(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
